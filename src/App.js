d17f37de72e29b935129c5618130acedd8aee3d4
charper95 <charper1995@gmail.com>
2021-07-27 14:50:57 -0700

    add end game if someone wins

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ class App extends Component{
       squares: ['0', '1', '2', '3', '4', '5', '6', '7', '8'],
       playerX: true,
       gamePlay: true,
+      click: true,
       circleWinning: false,
       crossWinning: false
     }
@@ -23,62 +24,65 @@ class App extends Component{
   }
 
   checkWin = (value, index) => {
-    const {squares} = this.state;
+    const {squares, click, gamePlay} = this.state;
     console.log(value)
     console.log(index)
     console.log(squares[index])
-    if (squares[0] === '❌' && squares[1] === '❌' && squares[2] === '❌') {
-      this.setState({crossWinning: true})
-    } else if (squares[3] === '❌' && squares[4] === '❌' && squares[5] === '❌') {
-      this.setState({crossWinning: true})
-    } else if (squares[6] === '❌' && squares[7] === '❌' && squares[8] === '❌') {
-      this.setState({crossWinning: true})
-    } else if (squares[6] === '❌' && squares[4] === '❌' && squares[2] === '❌') {
-      this.setState({crossWinning: true})
-    } else if (squares[0] === '❌' && squares[3] === '❌' && squares[6] === '❌') {
-      this.setState({crossWinning: true})
-    } else if (squares[1] === '❌' && squares[4] === '❌' && squares[7] === '❌') {
-      this.setState({crossWinning: true})
-    } else if (squares[2] === '❌' && squares[5] === '❌' && squares[8] === '❌') {
-      this.setState({crossWinning: true})
-    } else if (squares[0] === '❌' && squares[4] === '❌' && squares[8] === '❌') {
-      this.setState({crossWinning: true})
-    } else if (squares[0] === '⭕️' && squares[1] === '⭕️' && squares[2] === '⭕️') {
-      this.setState({circleWinning: true})
-    } else if (squares[3] === '⭕️' && squares[4] === '⭕️' && squares[5] === '⭕️') {
-      this.setState({circleWinning: true})
-    } else if (squares[6] === '⭕️' && squares[7] === '⭕️' && squares[8] === '⭕️') {
-      this.setState({circleWinning: true})
-    } else if (squares[0] === '⭕️' && squares[3] === '⭕️' && squares[6] === '⭕️') {
-      this.setState({circleWinning: true})
-    } else if (squares[1] === '⭕️' && squares[4] === '⭕️' && squares[7] === '⭕️') {
-      this.setState({circleWinning: true})
-    } else if (squares[2] === '⭕️' && squares[5] === '⭕️' && squares[8] === '⭕️') {
-      this.setState({circleWinning: true})
-    } else if (squares[0] === '⭕️' && squares[4] === '⭕️' && squares[8] === '⭕️') {
-      this.setState({circleWinning: true})
-    } else if (squares[6] === '⭕️' && squares[4] === '⭕️' && squares[2] === '⭕️') {
-      this.setState({circleWinning: true})
+    if(gamePlay) {
+      if (squares[0] === '❌' && squares[1] === '❌' && squares[2] === '❌') {
+        this.setState({crossWinning: true, click: false, gamePlay: false})
+      } else if (squares[3] === '❌' && squares[4] === '❌' && squares[5] === '❌') {
+        this.setState({crossWinning: true, click: false, gamePlay: false})
+      } else if (squares[6] === '❌' && squares[7] === '❌' && squares[8] === '❌') {
+        this.setState({crossWinning: true, click: false, gamePlay: false})
+      } else if (squares[6] === '❌' && squares[4] === '❌' && squares[2] === '❌') {
+        this.setState({crossWinning: true, click: false, gamePlay: false})
+      } else if (squares[0] === '❌' && squares[3] === '❌' && squares[6] === '❌') {
+        this.setState({crossWinning: true, click: false, gamePlay: false})
+      } else if (squares[1] === '❌' && squares[4] === '❌' && squares[7] === '❌') {
+        this.setState({crossWinning: true, click: false, gamePlay: false})
+      } else if (squares[2] === '❌' && squares[5] === '❌' && squares[8] === '❌') {
+        this.setState({crossWinning: true, click: false, gamePlay: false})
+      } else if (squares[0] === '❌' && squares[4] === '❌' && squares[8] === '❌') {
+        this.setState({crossWinning: true, click: false, gamePlay: false})
+      } else if (squares[0] === '⭕️' && squares[1] === '⭕️' && squares[2] === '⭕️') {
+        this.setState({circleWinning: true, click: false, gamePlay: false})
+      } else if (squares[3] === '⭕️' && squares[4] === '⭕️' && squares[5] === '⭕️') {
+        this.setState({circleWinning: true, click: false, gamePlay: false})
+      } else if (squares[6] === '⭕️' && squares[7] === '⭕️' && squares[8] === '⭕️') {
+        this.setState({circleWinning: true, click: false, gamePlay: false})
+      } else if (squares[0] === '⭕️' && squares[3] === '⭕️' && squares[6] === '⭕️') {
+        this.setState({circleWinning: true, click: false, gamePlay: false})
+      } else if (squares[1] === '⭕️' && squares[4] === '⭕️' && squares[7] === '⭕️') {
+        this.setState({circleWinning: true, click: false, gamePlay: false})
+      } else if (squares[2] === '⭕️' && squares[5] === '⭕️' && squares[8] === '⭕️') {
+        this.setState({circleWinning: true, click: false, gamePlay: false})
+      } else if (squares[0] === '⭕️' && squares[4] === '⭕️' && squares[8] === '⭕️') {
+        this.setState({circleWinning: true, click: false, gamePlay: false})
+      } else if (squares[6] === '⭕️' && squares[4] === '⭕️' && squares[2] === '⭕️') {
+        this.setState({circleWinning: true, click: false, gamePlay: false})
+      }
     }
-
-    
   }
 
   handleTurn = (index) => {
     const {squares} = this.state;
     const {playerX} = this.state;
-    if (playerX === true && squares[index] !== '⭕️'){
-      squares[index] = '❌'
-      this.setState({
-        squares: squares,
-        playerX: false
-      })
-    } else if (playerX === false && squares[index] !== '❌'){
-      squares[index] = '⭕️'
-      this.setState({
-        squares: squares,
-        playerX: true
-      })
+    const {click} = this.state;
+    if (click) {
+      if (playerX === true && squares[index] !== '⭕️'){
+        squares[index] = '❌'
+        this.setState({
+          squares: squares,
+          playerX: false
+        })
+      } else if (playerX === false && squares[index] !== '❌'){
+        squares[index] = '⭕️'
+        this.setState({
+          squares: squares,
+          playerX: true
+        })
+      }
     }
   }
 
